0676a3e64d36579bd8114d377b882d19b676e9af
Raymond <Rayjones2170@gmail.com>
2021-01-16 12:19:12 -0500

    Feature: Adds function to narrow Villian's range based on preflop actions

--- main.py
+++ main.py
@@ -14,40 +14,78 @@
 
 import datetime
 
-from poker.hand import Combo
+from poker.hand import Combo,Hand,Range
 from calculation import holdem_calc
 
 from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+#Narrows villians range by taking the preflop action as input
+#Hero will be RFI / vs. Raise / vs. 3-bet / 4-bet / etc. against x position to narrow ranges
+#Assumes GTO preflop 100BB deep and hero follows charts
+def narrowRange(action, villian_position):
+    #Button RFI range -> Villian is on the button and raises first
+    if action == "RFI" and villian_position == "BU":
+        return Range('22+,A2s+,K2s+,Q2s+,J2s+,T2s+,95s+,85s+,74s+,63s+,53s+,43s,A2o+,K8o+,Q8o+,J8o+,T8o+,97o+,87o,76o,65o,54o')
+    #CO RFI
+    #HJ RFI
+    #LJ RFI
 
+    #Button 3-bet
+    #CO 3-bet
+    #HJ 3-bet
+
+    #CO 4-bet
+    #Button 5-bet
+    return None
 @app.route('/')
 def root():
     return render_template('index.html')
-
 @app.route('/cards',methods = ['POST', 'GET'])
-def getCard():
-   if request.method == 'POST':
+def getOdds():
+     villian_hand = narrowRange("RFI","BU")
+     board = ["Qc", "Th", "9s"]
+     hero_hand = Combo('KsJc')
+
+     #Constant Variables
+     do_exact_calculation = True
+     verbose = True
+     run_one_simulation = 1
+     do_not_read_from_file = None
+
+     odds = holdem_calc.calculate_odds_villan(board, do_exact_calculation, 
+                        run_one_simulation, do_not_read_from_file , 
+                        hero_hand, villan_hand, 
+                        verbose, print_elapsed_time = True)
+     return str(odds[0].get("win"))
+"""
+    if request.method == 'POST':
       user = request.form['nm']
       return redirect(url_for('success',name = user))
-   else:
-        #user = request.args.get('nm')
-        board = ["Qc", "Th", "9s"]
-        villan_hand = None
-        exact_calculation = True
+    else:
+        villian_hand = request.args.get('villian-hand')
+        hero_hand = request.args.get('hero-hand')
+        board = request.args.get('board')
+        #Case: User doesn't submit valid data
+        if request.args.get('villian-hand') == None:
+            print("no information submitted...populating with dummy data")
+            board = ["Qc", "Th", "9s"]
+            villan_hand = narrowRange("RFI","BU")
+            hero_hand = Combo('KsJc')
+
+        #Constant Variables
+        do_exact_calculation = True
         verbose = True
-        num_sims = 1
-        read_from_file = None
+        run_one_simulation = 1
+        do_not_read_from_file = None
 
-        hero_hand = Combo('KsJc')
-
-        odds = holdem_calc.calculate_odds_villan(board, exact_calculation, 
-                            num_sims, read_from_file , 
+        odds = holdem_calc.calculate_odds_villan(board, do_exact_calculation, 
+                            run_one_simulation, do_not_read_from_file , 
                             hero_hand, villan_hand, 
                             verbose, print_elapsed_time = True)
-        print("Another test")
-        print(odds[0].get("win"))
-        return (str(odds[0].get("win")))
+        #print(odds[0].get("win"))
+        #print(str(odds[0].get("win")))
+        """
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
